29dd1443aa009f024c083dc8dc2659efa318712d
大島大河 <oshima.automate.growth@gmail.com>
2026-07-16 10:36:23 +0900

    CIバグを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
                 target: 'http://localhost:8787',
                 changeOrigin: true,
             },
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: resolve(__dirname, 'src/frontend/index.html'),
         },
     },
 });
